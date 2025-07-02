9dca627a9b2ef1724cfe65cba61179245878c758
Felipe dos Santos Bento <dsbfelipe@outlook.com>
2025-07-02 17:57:15 -0300

    feat: implement all custom enumerables

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,69 @@
 module Enumerable
   # Your code goes here
+  def my_all? 
+    self.my_each do |elem|
+      return false unless yield(elem)
+    end
+    true
+  end
+
+  def my_any?
+    self.my_each do |elem|
+      return true if yield(elem)
+    end
+    false
+  end
+
+  def my_none?
+    self.my_each do |elem|
+      return false unless !yield(elem)
+    end
+    true
+  end
+
+  def my_count
+    return self.length unless block_given?
+
+    count = 0
+    self.each do |elem|
+      if yield(elem)
+        count = count + 1
+      end
+    end
+    count
+  end
+
+  def my_inject(initial_value = nil)
+    array = to_a
+
+    if initial_value.nil?
+      sum = array[0]
+      array[1..-1].my_each { |elem| sum = yield(sum, elem)}
+    else
+      sum = initial_value
+      array.my_each { |elem| sum = yield(sum, elem)} 
+    end
+    sum 
+  end
+
+  def my_map
+    return to_enum(:my_map) unless block_given?
+
+    result = []
+    self.my_each do |elem|
+      result << yield(elem)
+    end
+    result
+  end
+
+  def my_select 
+    matches = []
+
+    self.my_each do |elem|
+      matches << elem if yield(elem)
+    end
+    matches
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +72,21 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
+    for i in 0...self.length
+      yield(self[i])
+    end 
+    self
+  end
+
+  def my_each_with_index
+    return to_enum(:my_each) unless block_given?
+
+    for i in 0...self.length
+      yield(self[i], i)
+    end
+    self
+  end
 end
